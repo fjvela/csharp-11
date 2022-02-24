79c2b9f10b010b196d33782e5e22d9bdf27be497
Javier Vela <fjvela@gmail.com>
2022-02-24 05:58:07 +0100

    C#11 raw string literals

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello human!");
+
+void RawStrings()
+{
+    Console.WriteLine("** Raw string literals **");
+    var myStringRaw = """
+          <element attr="content"/>
+          """;
+    var myString = @"
+          <element attr=""content""/>";
+    Console.WriteLine(myStringRaw);
+    Console.WriteLine(myString);
+    Console.WriteLine("** End raw string literals **");
+}
+
+
+RawStrings();
+
+Console.WriteLine("End");
+Console.ReadLine();
