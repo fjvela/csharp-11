6a8ab16164ac6db3c7b2d5a77de9d33e7ba22822
Javier Vela <fjvela@gmail.com>
2023-01-19 19:08:37 +0100

    Update example file scoped

---
+++ 9-file-scoped-types/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FileScopedTypes;
+
+Console.WriteLine("Hello human!");
+
+
+Widget widget= new Widget();
+Console.WriteLine(widget.ProvideAnswer());
+
+Console.ReadLine();
---
+++ 9-file-scoped-types/Widget.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileScopedTypes
+{
+    file interface IWidget
+    {
+        int ProvideAnswer();
+    }
+
+    file class HiddenWidget
+    {
+        public int Work() => 42;
+    }
+
+    public class Widget : IWidget
+    {
+        public int ProvideAnswer()
+        {
+            var worker = new HiddenWidget();
+            return worker.Work();
+        }
+    }
+
+}
